bd950591935a79e1e9ada19d0f0cbb94006448c6
MRPRO77 <mrpro77@hotmail.com>
2025-06-14 07:04:07 -0300

    Inicialização Função Millis

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,66 @@
-#include <Arduino.h>
+/*******************************************************************************
+   Título:      TCC  Engenharia Mecatrônica.
 
-// put function declarations here:
-int myFunction(int, int);
+   Descrição:   C L P   ---  I H M
+
+   Processador: ARDUINO Uno
+   
+   Geração Multifolhas - Função Millis
+
+
+   Desenvolvido: MRPRO Tecnologia e Automação
+
+   Autor: Tecnologo. Marcelo Rodrigues     Crea-SP 5070505617
+
+   Data de Inicio: 07/06/2025       Versão:   2025.1.0
+
+        
+
+ 
+*******************************************************************************/  
+/* ============================================================================  
+                                                              
+                                       _                      
+                                      / \                     
+                                     |oo >                    
+                                     _\=/_                    
+                    ___         #   /  _  \   #               
+                   /<> \         \\//|/.\|\\//                
+                 _|_____|_        \/  \_/  \/                 
+                | | === | |          |\ /|                    
+                |_|  0  |_|          \_ _/                    
+                 ||  0  ||           | | |                    
+                 ||__*__||           | | |                    
+                |* \___/ *|          []|[]                    
+                /=\ /=\ /=\          | | |                    
+________________[_]_[_]_[_]_________/_]_[_\___________________________________
+                                                                             
+============================================================================== */
+
+// ========================================================================================================
+// --- Bibliotecas Auxiliares ---
+
+#include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <avr/interrupt.h>
+#include <avr/io.h>
+
+
+#include "Config_mcu.h"
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ 
+
+            config_mcu ();
+
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+         Funcionamento_Sistema ();
+
+   }
